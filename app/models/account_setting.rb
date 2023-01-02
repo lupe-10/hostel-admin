87f84c1cd6bb88c5ddeeea82b47ba360c687a932
lupe-10 <lupe.diez@gmail.com>
2023-01-02 11:29:10 -0300

    WIP

--- app/models/account_setting.rb
+++ app/models/account_setting.rb
@@ -1,2 +1,3 @@
 class AccountSetting < ApplicationRecord
+  belongs_to :user
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
     root to: 'pages#home'
-  get 'owner', to: 'automobiles#owner', as: :owner
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # resources :automobiles do
   #   resources :bookings, only: %i[new create]
